2f685355fdcc2bd41c407bb35099d18ec250ae4b
Jackson Doherty <jackson@techne.xyz>
2024-04-07 12:55:12 -0400

    Remove attributes and re-exports taken from example program

--- programs-vanilla/field-authority-interface/src/lib.rs
+++ programs-vanilla/field-authority-interface/src/lib.rs
@@ -1,8 +1,5 @@
 //! Crate defining the field authority interface
 
-#![allow(clippy::arithmetic_side_effects)]
-#![cfg_attr(not(test), forbid(unsafe_code))]
-
 pub mod constant;
 pub mod error;
 pub mod helper;
@@ -11,7 +8,3 @@ pub mod state;
 
 pub use constant::*;
 pub use helper::*;
-
-// Export current sdk types for downstream users building with a different sdk version
-// Export borsh for downstream users
-pub use {borsh, solana_program};
