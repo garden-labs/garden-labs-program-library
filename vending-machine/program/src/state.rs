81f86acb52865a5716c7a5f8458c00c1c6385e7b
Jackson Doherty <jackson@techne.xyz>
2024-06-29 17:40:37 -0400

    Add additional fields

--- vending-machine/program/src/state.rs
+++ vending-machine/program/src/state.rs
@@ -3,14 +3,14 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(Default, Debug, InitSpace)]
 pub struct VendingMachineData {
-    pub admin: Pubkey, // 32
-                       // pub treasury: Pubkey,         // 32
-                       // pub max_supply: u16,          // 2
-                       // pub mint_price_lamports: u64, // 8
-                       // #[max_len(32)]
-                       // pub name_prefix: String, // 32
-                       // #[max_len(32)]
-                       // pub symbol: String, // 32
-                       // #[max_len(200)]
-                       // pub uri: String, // 200
+    pub admin: Pubkey,            // 32
+    pub treasury: Pubkey,         // 32
+    pub max_supply: u16,          // 2
+    pub mint_price_lamports: u64, // 8
+    #[max_len(32)]
+    pub name_prefix: String, // 32
+    #[max_len(32)]
+    pub symbol: String, // 32
+    #[max_len(200)]
+    pub uri_prefix: String, // 200
 }
--- vending-machine/test/vending-machine.ts
+++ vending-machine/test/vending-machine.ts
@@ -1,26 +1,39 @@
 import assert from "assert";
 
-import { workspace } from "@coral-xyz/anchor";
-import { Keypair } from "@solana/web3.js";
+import { workspace, BN } from "@coral-xyz/anchor";
+import { Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 import { setPayer } from "../../util/js/config";
 import { ANCHOR_WALLET_KEYPAIR } from "../../util/js/constants";
 import { VendingMachine } from "../../target/types/vending_machine";
 
 describe("Vending Machine", () => {
+  const admin = ANCHOR_WALLET_KEYPAIR;
+  const treasury = Keypair.generate();
+  const maxSupply = 10000;
+  const mintPriceLamports = 0.1 * LAMPORTS_PER_SOL;
+  const namePrefix = "My Test Collection";
+  const symbol = "TEST";
+  const uriPrefix = "https://arweave.net";
+
   it("Initialize", async () => {
     const { program } = setPayer<VendingMachine>(
       ANCHOR_WALLET_KEYPAIR,
       workspace.VendingMachine
     );
 
-    const admin = ANCHOR_WALLET_KEYPAIR.publicKey;
     const vendingMachineDataKp = Keypair.generate();
     const vendingMachineData = vendingMachineDataKp.publicKey;
 
     await program.methods
       .init({
-        admin,
+        admin: admin.publicKey,
+        treasury: treasury.publicKey,
+        maxSupply,
+        mintPriceLamports: new BN(mintPriceLamports.toString()),
+        namePrefix,
+        symbol,
+        uriPrefix,
       })
       .accountsPartial({
         vendingMachineData,
@@ -32,6 +45,12 @@ describe("Vending Machine", () => {
     const v = await program.account.vendingMachineData.fetch(
       vendingMachineData
     );
-    assert(v.admin.equals(admin));
+    assert(v.admin.equals(admin.publicKey));
+    assert(v.treasury.equals(treasury.publicKey));
+    assert.equal(v.maxSupply, maxSupply);
+    assert.equal(v.mintPriceLamports, mintPriceLamports);
+    assert.equal(v.namePrefix, namePrefix);
+    assert.equal(v.symbol, symbol);
+    assert.equal(v.uriPrefix, uriPrefix);
   });
 });
