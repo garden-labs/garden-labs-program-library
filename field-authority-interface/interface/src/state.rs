ea82ff577afb5f4a3169b0607cf38f0b4c605e7b
Jackson Doherty <jackson@gardenlabs.com>
2024-08-13 09:16:03 -0400

    Create field authority tlv struct similar to Token Metadata

--- field-authority-interface/interface/src/state.rs
+++ field-authority-interface/interface/src/state.rs
@@ -1,11 +1,20 @@
 use {
     borsh::{BorshDeserialize, BorshSerialize},
-    solana_program::pubkey::Pubkey,
+    solana_program::{
+        borsh1::{get_instance_packed_len, try_from_slice_unchecked},
+        program_error::ProgramError,
+        pubkey::Pubkey,
+    },
+    spl_discriminator::SplDiscriminate,
+    spl_token_metadata_interface::state::Field,
+    spl_type_length_value::{
+        state::{TlvState, TlvStateMut},
+        variable_len_pack::VariableLenPack,
+    },
 };
 
-// NOTE: Currently only a single field authority is supported via a PDA
-// We could extend this to support multiple field authorities and use a TLV structure to store them
-// That said, single authorities may be simpler for authority programs / PDAs
+// Legacy implementation
+
 #[derive(Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct FieldAuthorityAccount {
     pub authority: Pubkey,
@@ -14,5 +23,55 @@ pub struct FieldAuthorityAccount {
 impl FieldAuthorityAccount {
     pub fn space() -> usize {
         std::mem::size_of::<Pubkey>()
+    }
+}
+
+// Single account TLV implementation
+
+#[derive(Clone, Debug, PartialEq, BorshDeserialize, BorshSerialize)]
+pub struct FieldAuthority {
+    pub field: Field,
+    pub authority: Pubkey,
+}
+
+#[derive(Clone, Debug, PartialEq, BorshDeserialize, BorshSerialize, SplDiscriminate)]
+#[discriminator_hash_input("field_authorities")]
+pub struct FieldAuthorities {
+    pub authorities: Vec<FieldAuthority>,
+}
+impl VariableLenPack for FieldAuthorities {
+    fn pack_into_slice(&self, dst: &mut [u8]) -> Result<(), ProgramError> {
+        borsh::to_writer(&mut dst[..], self).map_err(Into::into)
+    }
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        try_from_slice_unchecked(src).map_err(Into::into)
+    }
+
+    fn get_packed_len(&self) -> Result<usize, ProgramError> {
+        get_instance_packed_len(self).map_err(Into::into)
+    }
+}
+impl FieldAuthorities {
+    pub fn add_field_authority(&mut self, field: Field, authority: Pubkey) {
+        for fa in &self.authorities {
+            if fa.field == field && fa.authority == authority {
+                return; // Exit early if a match is found
+            }
+        }
+        self.authorities.push(FieldAuthority { field, authority });
+    }
+
+    /// Removes the field authority pair. Returns true if the pair was found.
+    pub fn remove_field_authority(&mut self, field: Field, authority: Pubkey) -> bool {
+        let mut found = false;
+        self.authorities.retain(|fa| {
+            let should_retain = !(fa.field == field && fa.authority == authority);
+            if !should_retain {
+                found = true;
+            }
+            return should_retain;
+        });
+        return found;
     }
 }
