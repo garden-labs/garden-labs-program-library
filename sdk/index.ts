60dca91da79cd636df6b01b2879ab832c6ed74e1
Jackson Doherty <jackson@gardenlabs.com>
2024-09-02 21:23:53 -0400

    Move to npm, setup files to use as npm package, basic setup working

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,1 +1,3 @@
-export { ATM_PROGRAM_ID } from '../util/js/constants';
+export { default as VendingMachineIdl } from "../target/idl/vending_machine.json";
+export { ATM_PROGRAM_ID, ANCHOR_WALLET_KEYPAIR } from "../util/js/constants";
+export { VendingMachine } from "../target/types/vending_machine";
--- util/js/constants.ts
+++ util/js/constants.ts
@@ -7,10 +7,11 @@ import toml from "@iarna/toml";
 import { web3 } from "@coral-xyz/anchor";
 import { Keypair } from "@solana/web3.js";
 import expandTilde from "expand-tilde";
+import appRoot from "app-root-path";
 
 // Derive Anchor wallet keypair from Anchor.toml
 const ANCHOR_TOML_JSON = toml.parse(
-  readFileSync(path.join(__dirname, "..", "..", "Anchor.toml"), "utf-8")
+  readFileSync(path.join(appRoot.path, "Anchor.toml"), "utf-8")
 );
 const ANCHOR_WALLET_PATH = expandTilde(
   (ANCHOR_TOML_JSON.provider as { wallet: string }).wallet
@@ -20,25 +21,23 @@ export const ANCHOR_WALLET_KEYPAIR = Keypair.fromSecretKey(
 );
 
 export const BUILD_ATM_SCRIPT_PATH = path.join(
-  __dirname,
-  "..",
+  appRoot.path,
+  "util",
   "bash",
   "build",
   "advanced-token-metadata.sh"
 );
 
 export const DEPLOY_ATM_SCRIPT_PATH = path.join(
-  __dirname,
-  "..",
+  appRoot.path,
+  "util",
   "bash",
   "deploy",
   "advanced-token-metadata.sh"
 );
 
 const ATM_KEYPAIR_PATH = path.join(
-  __dirname,
-  "..",
-  "..",
+  appRoot.path,
   "target",
   "deploy",
   "advanced_token_metadata-keypair.json"
