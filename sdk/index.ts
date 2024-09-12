03a5800ae806c8f419157f8e9890ae10148ef8de
JD <jackson@gardenlabs.com>
2024-09-12 19:33:46 -0400

    Dev mode, expose more to sdk

--- sdk/index.ts
+++ sdk/index.ts
@@ -3,6 +3,10 @@ export { optimizeSimSignSendAndConf } from "../common/js";
 export { The100 } from "../target/types/the_100";
 export { ATM_PROGRAM_ID } from "../advanced-token-metadata/js";
 export { TREASURY_PUBLIC_KEY } from "../vending-machine/js";
+export {
+  MEMBER_PDA_SEED as THE100_MEMBER_PDA_SEED,
+  indexToSeed as the100IndexToSeed,
+} from "../the100/js";
 
 // Helpful for development but perhaps should remove from package eventually
 export { ANCHOR_WALLET_KEYPAIR } from "../util/js";
