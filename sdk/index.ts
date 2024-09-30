ffe58ef6571f7840e516db5ff6fbdb81435a0d7a
JD <jackson@gardenlabs.com>
2024-09-29 20:43:59 -0400

    Fix gitignore, add build script back, add target copy into sdk

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,6 +1,6 @@
-export { default as The100Idl } from "../target/idl/the_100.json";
+export { default as The100Idl } from "./target/idl/the_100.json";
 export { optimizeSimSignSendAndConf, randomStr, sleep } from "../common/js";
-export { The100 } from "../target/types/the_100";
+export { The100 } from "./target/types/the_100";
 export { ATM_PROGRAM_ID } from "../advanced-token-metadata/js";
 export { TREASURY_PUBLIC_KEY } from "../vending-machine/js";
 export {
