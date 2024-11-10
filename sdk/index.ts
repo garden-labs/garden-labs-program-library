c8c08c96db9c22907248c2cca53d598c5d50957a
Jackson Doherty <jackson@gardenlabs.com>
2024-11-10 16:53:42 -0500

    Rename mint fee

--- sdk/index.ts
+++ sdk/index.ts
@@ -6,7 +6,7 @@ export { TREASURY_PUBKEY } from "../vending-machine/js";
 export {
   MEMBER_PDA_SEED as THE100_MEMBER_PDA_SEED,
   indexToSeed as the100IndexToSeed,
-  getMintFeeLamports,
+  getMintFeeLamports as the100GetMintFeeLamports,
   TREASURY_PUBKEY as THE100_TREASURY_PUBKEY,
   COL_DATA_PUBKEY,
 } from "../the100/js";
